144c3f92d5fb02d52d2df0cee43623cb1343de02
prefetchnta <crhackos@gmail.com>
2017-04-27 17:23:08 +0800

    svn 2324

--- usr/arm-fsl-linux-gnueabi.sh
+++ usr/arm-fsl-linux-gnueabi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../mak
-PATH=/usr/local/fsl-linaro-toolchain/bin:$PATH
+export PATH=/usr/local/fsl-linaro-toolchain/bin:$PATH
 ./linux.sh -march=armv7-a -Wl,--no-wchar-size-warning arm-fsl-linux-gnueabi-
 cd ../usr
