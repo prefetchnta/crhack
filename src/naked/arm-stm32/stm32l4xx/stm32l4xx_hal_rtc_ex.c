4dea055b64cdb3de06e7c2efeca47f99c009a988
prefetchnta <crhackos@gmail.com>
2019-04-01 16:49:24 +0800

    svn 4845

--- src/naked/arm-stm32/stm32l4xx/stm32l4xx_hal_rtc_ex.c
+++ src/naked/arm-stm32/stm32l4xx/stm32l4xx_hal_rtc_ex.c
@@ -909,7 +909,7 @@ HAL_StatusTypeDef HAL_RTCEx_SetWakeUpTimer_IT(RTC_HandleTypeDef *hrtc, uint32_t 
     __HAL_RTC_WAKEUPTIMER_EXTI_ENABLE_IT();
   }
 #else /* defined(STM32L412xx) || defined(STM32L422xx) */
-    __HAL_RTC_WAKEUPTIMER_EXTI_ENABLE_EVENT();
+    __HAL_RTC_WAKEUPTIMER_EXTI_ENABLE_IT();
 #endif /* defined(STM32L412xx) || defined(STM32L422xx) */
 
   __HAL_RTC_WAKEUPTIMER_EXTI_ENABLE_RISING_EDGE();
