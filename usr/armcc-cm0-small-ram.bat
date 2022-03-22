396272c37840f399ce619b2100ada0e690c9cbdc
prefetchnta <crhackos@gmail.com>
2022-03-22 15:04:02 +0800

    svn 8508

---
+++ usr/armcc-cm0-small-ram.bat
@@ -0,0 +1,6 @@
+@echo off
+cd ..\mak
+call mdk50_32.bat "--cpu Cortex-M0 -D_CR_USE_FP32_ -D_BJNETCOMM_SMALL_ROM_ -D_BJNETCOMM_SMALL_RAM_ -D_CR_NAKED_NO_WSTRING_"
+md ..\bin\arm-cm0-small-ram
+move ..\bin\armcc_naked_arm\*.a ..\bin\arm-cm0-small-ram
+cd ..\usr
