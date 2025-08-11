709a12d86c751e576215073f27dbd7004579b5b0
prefetchnta <crhackos@gmail.com>
2025-08-11 17:31:59 +0800

    svn 14401

--- usr/n3292x.sh
+++ usr/n3292x.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../mak
-./linux.sh "-D_GNU_SOURCE=1 -D_CR_NO_LINUX_I2C_ -D_CR_NO_LINUX_SPI_ -D_CR_NO_FAST_ATOM_ -D_CR_USE_SYNC_LOCK_RELEASE_ -DLUA_C89_NUMBERS" -lgcc arm-linux-
+./linux.sh "-D_GNU_SOURCE -D_CR_NO_LINUX_I2C_ -D_CR_NO_LINUX_SPI_ -D_CR_NO_FAST_ATOM_ -D_CR_USE_SYNC_LOCK_RELEASE_ -DLUA_C89_NUMBERS" -lgcc arm-linux-
 cd ../usr
